fb0e7e2716aff8f1885f4d8ee6e084582e5250fc
alanmaranto <alanarostegui@hotmail.com>
2019-12-28 15:31:01 -0600

    material ui pokelist

--- src/containers/PokeList.js
+++ src/containers/PokeList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import List from '../components/List/List';
 import axios from 'axios';
+import { AppBar, Typography, Toolbar } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 
 class PokeList extends Component {
 
@@ -21,11 +23,23 @@ class PokeList extends Component {
     }
 
     render() {
+        const { classes } = this.props;
         const { pokeData } = this.state; 
-        return ( 
-            <List pokeData={pokeData} />
+        return (
+            <> 
+                <AppBar className={classes.NavColor} position="static">
+                    <Toolbar>
+                        <Typography variant="h6" component="p" >PokeApp</Typography>
+                    </Toolbar>
+                </AppBar>
+                <List pokeData={pokeData} />
+            </>
          );
     }
 }
  
-export default PokeList;
+export default withStyles({
+    NavColor: {
+        backgroundColor: '#db0921'
+    }
+})(PokeList);
