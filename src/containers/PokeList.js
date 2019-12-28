da9654d283cf3f87d953595a25c165f99c21dfa7
alanmaranto <alanarostegui@hotmail.com>
2019-12-28 12:41:54 -0600

    pokelist

---
+++ src/containers/PokeList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import List from '../components/List/List';
+
+class PokeList extends Component {
+    render() { 
+        return ( 
+            <List />
+         );
+    }
+}
+ 
+export default PokeList;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "../containers/Home";
 import { BrowserRouter } from "react-router-dom";
+import PokeList from "../containers/PokeList";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path='/pokemons' component={PokeList} />
       </Switch>
     </BrowserRouter>
   );
