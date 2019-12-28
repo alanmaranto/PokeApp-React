3f1508f0ab6a2f3036f0597bd62ad3d416e61eaf
alanmaranto <alanarostegui@hotmail.com>
2019-12-28 12:34:36 -0600

    agregando imagen

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header">
+        <img src={process.env.PUBLIC_URL + '/images/pokemon.png'} width="400" alt="poke" />
         <h1>Poke App</h1>
+        <button className="btn btn-primary">Ver Pokemon</button>
       </header>
     </div> 
    );
