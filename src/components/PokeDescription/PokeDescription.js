1a8d29350edfb37111c170a53862dea3177d01b0
alanmaranto <alanarostegui@hotmail.com>
2019-12-30 14:38:43 -0600

    mostrando la informacion

--- src/components/PokeDescription/PokeDescription.js
+++ src/components/PokeDescription/PokeDescription.js
@@ -1,14 +1,42 @@
 import React from "react";
-import PokeCard from '../PokeCard/PokeCard';
-import Grid from '@material-ui/core/Grid';
+import PokeCard from "../PokeCard/PokeCard";
+import { Grid, Paper, Typography } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
 
-const PokeDescription = ({ pokeImage, pokeName }) => {
+const PokeDescription = ({ pokeImage, pokeName, pokeDescription, classes }) => {
   return (
-    <Grid container justify="center">
-      <PokeCard image={pokeImage} pokeName={pokeName}>
-      </PokeCard>
+    <Grid container className={classes.centerContainer} justify="center">
+      <Grid item md={6}>
+        <PokeCard image={pokeImage} pokeName={pokeName} />
+      </Grid>
+      <Grid item md={6}>
+        <div className={classes.descriptionContainer}>
+          <Paper className={classes.descriptionBox}>
+            <Typography variant="body1" component="p" className={classes.descriptionText}>
+              {pokeDescription}
+            </Typography>
+          </Paper>
+        </div>
+      </Grid>
     </Grid>
   );
 };
 
-export default PokeDescription;
+export default withStyles({
+  centerContainer: {
+    margin: "0 auto",
+    width: "60% !important"
+  },
+  descriptionContainer: {
+    margin: "2em"
+  },
+  descriptionBox: {
+    padding: "2em",
+    height: "auto"
+  },
+  descriptionText: {
+    fontSize: "1em",
+    textAlign: "justify",
+    fontFamily: "Verdana"
+  }
+})(PokeDescription);
--- src/containers/PokeInfo.js
+++ src/containers/PokeInfo.js
@@ -1,28 +1,50 @@
 import React, { Component } from "react";
 import PokeDescription from "../components/PokeDescription/PokeDescription";
+import AppNav from '../components/AppNav/AppNav';
 
-import Axios from 'axios';
+import Axios from "axios";
+
+const initialState = {
+  pokemonDescription: undefined
+};
 
 class PokeInfo extends Component {
-    componentDidMount(){
-      //peticion con axios
-      const { match } = this.props;
-      const descriptionId = match.params.pokeIndex;
-      const pokeDescriptionUrl = `${process.env.REACT_APP_POKE_API_BASE_URL}pokemon-species/${descriptionId}/`;
-      Axios.get(pokeDescriptionUrl)
-        .then(res => console.log(res.data.flavor_text_entries[11].flavor_text))
-    }
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+
+  componentDidMount() {
+
+    //peticion con axios
+    const { match } = this.props;
+    const descriptionId = match.params.pokeIndex;
+    const pokeDescriptionUrl = `${process.env.REACT_APP_POKE_API_BASE_URL}pokemon-species/${descriptionId}/`;
+    Axios.get(pokeDescriptionUrl).then(res => {
+      const { flavor_text_entries } = res.data;
+      this.setState({
+        pokemonDescription: flavor_text_entries[11].flavor_text
+      });
+      console.log(this.state.pokemonDescription)
+    });
+  }
 
   render() {
     const { match } = this.props;
     const url =
-    "https://github.com/PokeApi/sprites/blob/master/sprites/pokemon/other-sprites/official-artwork/";
+      "https://github.com/PokeApi/sprites/blob/master/sprites/pokemon/other-sprites/official-artwork/";
     const pokeId = match.params.pokeIndex;
     const pokeName = match.params.pokeName;
+    const { pokemonDescription } = this.state;
 
     return (
       <>
-        <PokeDescription pokeImage={`${url}${pokeId}.png?raw=true`} pokeName={pokeName} />
+        <AppNav />
+        <PokeDescription
+          pokeImage={`${url}${pokeId}.png?raw=true`}
+          pokeName={pokeName}
+          pokeDescription={pokemonDescription}
+        />
       </>
     );
   }
