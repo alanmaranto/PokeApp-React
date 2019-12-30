18ab2c4275a78579cd428ea0bf6529f23163bf1f
alanmaranto <alanarostegui@hotmail.com>
2019-12-30 14:55:24 -0600

    refactor pokeinfo

--- src/containers/PokeInfo.js
+++ src/containers/PokeInfo.js
@@ -5,7 +5,10 @@ import AppNav from '../components/AppNav/AppNav';
 import Axios from "axios";
 
 const initialState = {
-  pokemonDescription: undefined
+  pokemonDescription: undefined,
+  pokeName: undefined,
+  pokeId: undefined
+
 };
 
 class PokeInfo extends Component {
@@ -18,24 +21,23 @@ class PokeInfo extends Component {
 
     //peticion con axios
     const { match } = this.props;
-    const descriptionId = match.params.pokeIndex;
-    const pokeDescriptionUrl = `${process.env.REACT_APP_POKE_API_BASE_URL}pokemon-species/${descriptionId}/`;
+    const pokeId = match.params.pokeIndex;
+    const pokeName = match.params.pokeName;
+    const pokeDescriptionUrl = `${process.env.REACT_APP_POKE_API_BASE_URL}pokemon-species/${pokeId}/`;
     Axios.get(pokeDescriptionUrl).then(res => {
       const { flavor_text_entries } = res.data;
       this.setState({
-        pokemonDescription: flavor_text_entries[11].flavor_text
+        pokemonDescription: flavor_text_entries[11].flavor_text,
+        pokeName,
+        pokeId
       });
       console.log(this.state.pokemonDescription)
     });
   }
 
   render() {
-    const { match } = this.props;
-    const url =
-      "https://github.com/PokeApi/sprites/blob/master/sprites/pokemon/other-sprites/official-artwork/";
-    const pokeId = match.params.pokeIndex;
-    const pokeName = match.params.pokeName;
-    const { pokemonDescription } = this.state;
+    const url = `${process.env.REACT_APP_POKEMON_ART}`
+    const { pokemonDescription, pokeName, pokeId } = this.state;
 
     return (
       <>
